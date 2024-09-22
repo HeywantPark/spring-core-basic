1bef92adcdca99a17cfc1a22583c740850e33ffb
Park Hyewon <db1648@naver.com>
2024-09-22 22:49:12 +0900

    240922 빈등록 초기 , 소멸 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -28,15 +28,11 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     public void disconnect(){
         System.out.println("close : " + url);
     }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메세지");
     }
-
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         disconnect();
     }
 }
